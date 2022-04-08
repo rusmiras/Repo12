e51d772be68f246ea3d4112f757c527c0d65dfc3
Rusmira Sabic <sabic.rusmira@gmail.com>
2022-04-08 15:18:01 +0200

    Sum of fibonacci numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,23 @@
+// Only change code below this line
+function sumFibonacci(num) {
+    if (num <= 0) {
+        return 0;
+    } else {
+        var fib = [0, 1];
+        var sum = 1;
+        for (var i = 2; i <= num; i++){
+            fib[i] = fib[i-1] + fib[i - 2];
+            if (fib[i] % 2 == 1 && fib[i] <= num) {
+                sum += fib[i];
+            }
+        }
+        return sum;
+    }
+}
+// Only change code above this line
+console.log(sumFibonacci(1)); // Change this line
+console.log(sumFibonacci(10)); // Change this line
+console.log(sumFibonacci(20)); // Change this line
+console.log(sumFibonacci(4)); // Change this line
+console.log(sumFibonacci(-5)); // Change this line
+module.exports = sumFibonacci;
